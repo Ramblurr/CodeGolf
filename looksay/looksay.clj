b131c2d534bb3a29c95fe9dfb005177d1432c935
Casey Link <unnamedrambler@gmail.com>
2011-05-01 16:10:20 -0400

    remove all digits

--- looksay/looksay.clj
+++ looksay/looksay.clj
@@ -1,20 +1,24 @@
+(def z `~(count ""))
+(def ten (read-string (str z "xa")))
 (defn num->digits [n]
-  """ Takes a number and returns a sequence containing
-      its digits in ascending order
-      e.g., 123 -> (1 2 3)"""
- (loop [num n, digits ()] (if (zero? num) (seq digits) (recur (quot num 10) (cons (mod num 10) digits)))))
+  " Takes a number and returns a sequence containing
+    its digits in ascending order
+    e.g., 123 -> (1 2 3)"
+ (loop [num n, digits ()] (if (zero? num) (seq digits) (recur (quot num ten) (cons (mod num ten) digits)))))
 (defn digits->num [seq]
-  """ Takes a sequence of digits and returns the number
-      they represent
-      e.g., (1 2 3) -> 123 """
-  (reduce (fn [a b](+ b (* a 10))) seq))
+  " Takes a sequence of digits and returns the number
+    they represent
+    e.g., (1 2 3) -> 123 "
+  (reduce (fn [a b](+ b (* a ten))) seq))
 
 (defn look-seq [n]
+  " Returns a sequence containing the look-say representaiton for n"
     (digits->num (mapcat (juxt count first) (partition-by identity (num->digits n)))))
 
-(defn look-n-say [n]
-  """ Returns a lazy sequence representing
-    Conway's look-and-say sequence starting at n """
-  (iterate look-seq n))
+(defn look-n-say
+  " Returns a lazy sequence representing
+    Conway's look-and-say sequence starting at n"
+  ([] (look-n-say (inc z)))
+  ([n] (iterate look-seq n)))
 
-(println (take 5 (look-n-say 1)))
+(println (take (+ ten ten) (look-n-say)))
